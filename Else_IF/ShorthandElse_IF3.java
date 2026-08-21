099665ab9995dbf87d9d2ec44c249af86627ab3e
mubsalman2-oss <mubsalman2@gmail.com>
2026-08-21 18:57:01 +0600

    Add files via upload

---
+++ Else_IF/ShorthandElse_IF3.java
@@ -0,0 +1,8 @@
+public class ShorthandElse_IF3 {
+    
+public static void main(String[] args) {   
+    int time = 20;
+    System.out.println((time < 18) ? "Good day." : "Good evening.");
+  }
+}
+
