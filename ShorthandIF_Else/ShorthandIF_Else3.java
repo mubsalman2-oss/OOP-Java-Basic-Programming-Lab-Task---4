202e1238e754fb42f4338b967511ad4cdd6da493
mubsalman2-oss <mubsalman2@gmail.com>
2026-08-21 18:59:46 +0600

    Add files via upload

---
+++ ShorthandIF_Else/ShorthandIF_Else3.java
@@ -0,0 +1,8 @@
+public class ShorthandIF_Else3 {
+    public static void main(String[] args) {   
+    int time = 20;
+    System.out.println((time < 18) ? "Good day." : "Good evening.");
+  }
+}
+    
+
