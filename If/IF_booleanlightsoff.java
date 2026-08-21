9bb4429403398b3ae9ec77d83154f775be0154dd
mubsalman2-oss <mubsalman2@gmail.com>
2026-08-21 18:54:59 +0600

    Add files via upload

---
+++ If/IF_booleanlightsoff.java
@@ -0,0 +1,11 @@
+public class IF_booleanlightsoff {
+  public static void main(String[] args) {
+    boolean isLightOn = false;
+
+    if (isLightOn) {
+      System.out.println("The light is on.");  
+    }
+    
+    System.out.println("This line always runs.");
+  }
+}
