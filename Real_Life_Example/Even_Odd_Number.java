abfb940eb0638c21601ab1bc903e5c8bb6ee11b7
mubsalman2-oss <mubsalman2@gmail.com>
2026-08-21 19:06:53 +0600

    Add files via upload

---
+++ Real_Life_Example/Even_Odd_Number.java
@@ -0,0 +1,12 @@
+public class Even_Odd_Number {
+     public static void main(String[] args) {
+    int myNum = 5;
+
+    if (myNum % 2 == 0) {
+      System.out.println(myNum + " is even");
+    } else {
+      System.out.println(myNum + " is odd");
+    } 
+  }
+    
+}
---
+++ Real_Life_Example/Example5.java
@@ -0,0 +1,17 @@
+public class Example5 {
+    public static void main(String[] args) {
+    int dice = 1;
+
+    while (dice <= 6) {
+      if (dice < 6) {
+        System.out.println("No Yatzy.");
+      } else {
+        System.out.println("Yatzy!");
+      }
+      dice = dice + 1;
+    }
+  }
+}
+
+    
+
